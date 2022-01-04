4ca3fa05a9f44ab8369c449f61474bb95fa20362
Nur Azizah <nur.azizah.7241@gmail.com>
2022-01-05 02:11:07 +0800

    updatemigrationmyuser

--- database/migrations/2021_12_06_053905_create_fis8_myusers_table.php
+++ database/migrations/2021_12_06_053905_create_fis8_myusers_table.php
@@ -24,7 +24,7 @@ class CreateFis8MyusersTable extends Migration
             $table->enum('is_active', ['0', '1'])->default('1');
             $table->enum('is_admin', ['0', '1'])->default('0');
             $table->bigInteger('ticket')->default(5);
-            $table->bigInteger('score')->default(0);
+            $table->bigInteger('quiz_score')->default(0);
             $table->bigInteger('money')->default(200);
             $table->text('photo')->nullable();
 
