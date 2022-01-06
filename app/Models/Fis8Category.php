3cb1144bf51db044b5507521f28696830a5b457c
Donny The newspaper <Donnykurniawan1119@gmail.com>
2022-01-06 10:40:00 +0700

    Category

--- app/Models/Fis8Category.php
+++ app/Models/Fis8Category.php
@@ -13,6 +13,21 @@ class Fis8Category extends Model
         'id',
     ];
 
+    public function scopeSearch($query, $term)
+    {
+        $term = "%$term%";
+        $query->where(function ($query) use ($term) {
+            $query->where('id', 'like', $term)
+            ->orWhere('name', 'like', $term)
+            ->orWhere('description', 'like', $term)
+            ->orWhereHas('myUser', function ($query) use ($term) {
+                $query->where('id', 'like', $term);
+            });
+        });
+    }
+
+    
+
     public function levels()
     {
         return $this->hasMany(Fis8Level::class, 'fis8_category_id');
